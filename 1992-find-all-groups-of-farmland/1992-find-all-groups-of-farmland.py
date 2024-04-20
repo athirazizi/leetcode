caa3ab23c29f24b76de80ad10d72fd3184af7c79
Athir Azizi <108275763+athirazizi@users.noreply.github.com>
2024-04-20 20:28:10 +0100

    Create     1992-find-all-groups-of-farmland.py

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findFarmland(self, land):
+        """
+        :type land: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        m, n = len(land), len(land[0])
+        res = []
+        for i in range(m):
+            for j in range(n):
+                if land[i][j] == 1:
+                    bottom, right = i, j
+                    while bottom < m and land[bottom][j] == 1:
+                        bottom += 1
+                    while right < n and land[i][right] == 1:
+                        right += 1
+                    for x in range(i, bottom):
+                        for y in range(j, right):
+                            land[x][y] = 0
+                    res.append([i, j, bottom - 1, right - 1])
+        return res
